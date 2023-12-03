4d5ff7e2202601400a3a4d1cc483fac021376714
Himanshu Yadav <hy815088@gmail.com>
2023-12-03 18:48:36 +0530

    Added necessary code comments for plotting in q2

--- q2.m
+++ q2.m
@@ -3,7 +3,8 @@ mu = 0.014;
 filterLength = 300;
 
 % Easy File
-[echoedSignal, fs] = audioread('Audio/q2_easy.wav');
+FileName = 'Audio/q2_easy.wav';
+[echoedSignal, fs] = loadAudioFile(FileName);
 
 flag = 0;
 for k = 2:length(echoedSignal)
@@ -63,7 +64,8 @@ title('Output Signal (After Adaptive Filtering)');
 grid on;
 
 % Not So Easy File
-[echoedSignal, fs] = audioread('Audio/q2_not_so_easy.wav');
+FileName = 'Audio/q2_not_so_easy.wav';
+[echoedSignal, fs] = loadAudioFile(FileName);
 
 flag = 0;
 for k = 2:length(echoedSignal)
@@ -81,8 +83,25 @@ for k = 2:length(echoedSignal)
     end
 end
 
+flag2 = 0;
+for k = temp1:length(echoedSignal)
+    if (echoedSignal(k) == 0 && flag2 == 0)
+        for k1 = 1:1000
+            if (echoedSignal(k+k1)~=0)
+                break;
+            elseif (k1 == 1000)
+                flag2 = k;
+            end
+        end
+    elseif (flag2 ~= 0 && echoedSignal(k) ~= 0)
+        temp2 = k;
+        break;
+    end
+end
+
 originalSignal = echoedSignal(1:flag-1);
-echoedSignal = echoedSignal(temp1:end);
+% echoedSignal = [echoedSignal(temp1:flag2-1); zeros(length(echoedSignal)-flag2+1,1)];
+echoedSignal = echoedSignal(temp2:end);
 originalSignal = [originalSignal; zeros(length(echoedSignal) - length(originalSignal), 1)];
 
 originalSignal = originalSignal';
@@ -103,6 +122,7 @@ audiowrite('Audio/q2_EchoFreeNotSoEasy.wav', outputSignal, fs);
 figure(2);
 subplot(3, 1, 1);
 plot(echoedSignal);
+% xlim([0,140000]);
 xlabel('n');
 ylabel('x_{echoed}[n]');
 title('Input Signal with Echo');
@@ -110,6 +130,7 @@ grid on;
 
 subplot(3, 1, 2);
 plot(originalSignal);
+% xlim([0,140000]);
 xlabel('n');
 ylabel('x_{Desired}[n]');
 title('Desired Signal Without Echo');
@@ -117,6 +138,7 @@ grid on;
 
 subplot(3, 1, 3);
 plot(outputSignal);
+% xlim([0,140000]);
 xlabel('n');
 ylabel('y[n]');
 title('Output Signal (After Adaptive Filtering)');
