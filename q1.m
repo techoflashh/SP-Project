d34565e82aac4173d05d192411a4103ca915ef95
Himanshu Yadav <hy815088@gmail.com>
2023-12-03 09:33:49 +0530

    some progress in q2

--- q1.m
+++ q1.m
@@ -1,13 +1,13 @@
 clear vars;
 
 % Give the FileName Here
-FileName = "./Audio/q1_hard.wav";
+FileName = "./Audio/q1.wav";
 
 % Loading the audio file using the function loadAudioFile
 [y,Fs] = loadAudioFile(FileName);
 
 % Give the parameters Delay(in seconds) a(attenuation factor taking values in (0,1))
-Delay=3;
+Delay=10;
 a=0.65;
 
 % Creating the echoed signal using the function echoCreation
--- q2.m
+++ q2.m
@@ -1,8 +1,7 @@
 clearvars;
 mu = 0.01;
-filterLength = 64;
 
-[noisySignal,fs] = audioread("Audio/q2_easy.wav");
+[noisySignal,fs] = audioread("Audio/q2_not_so_easy.wav");
 
 for k = 2:length(noisySignal)
     if noisySignal(k) == 0
@@ -12,6 +11,7 @@ for k = 2:length(noisySignal)
 end
 
 originalSignal = [noisySignal(1:temp-1); zeros(length(noisySignal)-temp+1,1)];
+filterLength = length(originalSignal);
 
 t = 0:1/fs:2;
 
@@ -20,19 +20,18 @@ noisySignal = noisySignal';
 
 w = zeros(1, filterLength);
 
-for n = filterLength:length(noisySignal)
-    x = noisySignal(n:-1:n-filterLength+1);
-    xt = x';
+for n = 1:100
+    % x = noisySignal(n:-1:n-filterLength+1);
+    xt = originalSignal';
     y = w * xt;
-    e = originalSignal(n) - y;
+    e = originalSignal(n) - y(n);
     % mu = 1/(x * xt);
-    w = w + 2 * mu * e * x;
+    w = w + 2 * mu * e .* x;
 end
 
 outputSignal = filter(w, 1, noisySignal);
-% outputSignal = outputSignal';
 
-audiowrite('Audio/q2_tempEchoFreeEasy.wav', outputSignal, fs);
+% audiowrite('Audio/q2_tempEchoFreeEasy.wav', outputSignal, fs);
 
 figure;
 subplot(3,1,1); plot(originalSignal); title('Original Signal');
